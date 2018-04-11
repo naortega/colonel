4170a3d5aed4c010e8f554afe60dfdecf4f5a89b
Nicolás Ortega Froysa <nortega@themusicinnoise.net>
2018-04-11 20:53:38 +0200

    Properly check the compiler.

--- toolchains/i686-elf.cmake
+++ toolchains/i686-elf.cmake
@@ -2,5 +2,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR i686)
 
 set(CMAKE_C_COMPILER i686-elf-gcc)
-set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_C_FLAGS "-ffreestanding -fno-builtin" CACHE STRING "C flags for test compilation.")
+set(CMAKE_EXE_LINKER_FLAGS "-ffreestanding -nostdlib" CACHE STRING "Linker flags for test compilation.")
+set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_C_FLAGS)
 set(CMAKE_ASM_COMPILER i686-elf-as)
