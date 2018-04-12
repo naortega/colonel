003e3d96044838fa120ac531709fc73699e1926c
Nicolás Ortega Froysa <nortega@themusicinnoise.net>
2018-04-12 13:49:56 +0200

    Create a variable for 64-bit to check uint64_t.

--- toolchains/i686-elf.cmake
+++ toolchains/i686-elf.cmake
@@ -6,3 +6,5 @@ set(CMAKE_C_FLAGS "-ffreestanding -fno-builtin" CACHE STRING "C flags for test c
 set(CMAKE_EXE_LINKER_FLAGS "-ffreestanding -nostdlib" CACHE STRING "Linker flags for test compilation.")
 set(CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_C_FLAGS)
 set(CMAKE_ASM_COMPILER i686-elf-as)
+
+set(64BIT FALSE CACHE BOOL "Whether the architecture supports 64-bit.")
